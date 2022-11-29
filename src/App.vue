aa4d290ead31eb30b46b786fe98993d151afde47
Elsti97 <elsti.shehati@hotmail.it>
2022-11-29 16:03:34 +0100

    Inserimento file iniziali

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <HeaderComp />
+    <MainComp />
+  </div>
+</template>
+
+<script>
+import HeaderComp from './components/HeaderComp.vue'
+import MainComp from './components/MainComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp,
+    MainComp,
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
